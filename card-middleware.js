8ed78f7b7d55cc955350e8a67672357f67266174
Geovani <geovanimendoza.h@gmail.com>
2020-09-26 19:16:55 -0400

    feat: add card cases

--- card-middleware.js
+++ card-middleware.js
@@ -1,17 +1,21 @@
 var card1 = '2222222222222222222';
 var card2 = '3333333333333333333';
 var card3 = '6666222211115555000';
+var card4 = '4334000000000000777';
+var card5 = '2112000000000000555';
 var pin = 9999;
 module.exports = (req, res, next) => {
     if (req.method == 'POST' && req.path == '/card') {
         if ((req.body.cardNumber === card1 ||
             req.body.cardNumber === card2 ||
-            req.body.cardNumber === card3) && req.body.pinNumber === pin) {
+            req.body.cardNumber === card3 ||
+            req.body.cardNumber === card4 ||
+            req.body.cardNumber === card5) && req.body.pinNumber === pin) {
             if (req.body.attempts >= 4) {
                 res.status(400).json({
-                    message: 'User blocked',
-                    error: 1000
-                })
+                    message: 'card is blocked till tomorrow',
+                    error: 1500
+                })//
             } else {
                 switch (req.body.cardNumber) {
                     case card1: // jhon doe
@@ -51,7 +55,19 @@ module.exports = (req, res, next) => {
                                 answer3: 'casó',
                                 username: 'sam@gmail.com'
                             }
-                        })// mary
+                        })
+                        break;
+                    case card4:
+                        res.status(400).json({
+                            message: 'User not registered',
+                            error: 1400
+                        })//none
+                        break;
+                    case card5:
+                        res.status(400).json({
+                            message: 'card is blocked till tomorrow',
+                            error: 1500
+                        })//none
                         break;
                 }
             }
